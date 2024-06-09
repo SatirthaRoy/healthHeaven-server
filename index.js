a0145e93f3cfaa37d07f3500f8884d0fcb2b5bcd
himu <satirtharoy2003@gmail.com>
2024-06-09 13:28:07 +0600

    vercel deployed

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 5000;
 
 // used middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173']
+}));
 
 
 // middlewares
@@ -404,9 +406,9 @@ async function run() {
 
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
